55f5963801289faf74bb7c51683f5cae1aec1d41
Sanford Winslow <sanfordwinslow@Sanfords-MacBook-Pro.local>
2019-04-23 16:22:41 -0700

    adding week 3 assignment

---
+++ Agster/app/src/main/java/com/san/agster/ZombieMovies.java
@@ -0,0 +1,13 @@
+package com.san.agster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZombieMovies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zombie_movies);
+    }
+}
