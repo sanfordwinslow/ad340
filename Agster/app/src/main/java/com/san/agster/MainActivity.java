47bb36a9dc52f2f9a98b445a2a8387b1370b13fb
Captain <canaimax@yahoo.com>
2019-05-06 17:48:34 -0700

    Captain adding ABOUT activity, new strings, menu added info to ABOUT, add label to other activities and UP navigation

--- Agster/app/src/main/java/com/san/agster/MainActivity.java
+++ Agster/app/src/main/java/com/san/agster/MainActivity.java
@@ -59,8 +59,14 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.item_info:
+                Intent intent = new Intent(this, About.class);
+                startActivity(intent);
 
-        //noinspection SimplifiableIfStatement
+
+        }
+                //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
